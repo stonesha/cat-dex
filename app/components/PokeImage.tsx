f78ecb27e949ec019841da54518e724cfe330cd4
stonesha <stoneshaus@gmail.com>
2024-01-24 20:29:50 -0800

    removed console.log

--- app/components/PokeImage.tsx
+++ app/components/PokeImage.tsx
@@ -15,8 +15,7 @@ export default function PokeImage({ src, pokemon_name }: Props) {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-9">
             <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
           </svg> :
-          <img src={src} alt={`${pokemon_name} sprite`} onError={(e) => {
-            console.log(e)
+          <img src={src} alt={`${pokemon_name} sprite`} onError={() => {
             setError(true)
           }} />
       }
